72ee005f32a60cca1cb56d1aaa80c072ee4af879
Tomoya Nagasawa <ocrybit@gmail.com>
2025-10-11 02:53:04 +0400

    [weavedb] send all requests to wdb-cu

--- src/dev_weavedb.erl
+++ src/dev_weavedb.erl
@@ -8,13 +8,12 @@
 get_message(Msg1, Msg2, Opts) ->
     ProcID = hb_ao:get(<<"pid">>, Msg2, Opts),
     Slot = hb_ao:get(<<"slot">>, Msg2, Opts),
-    
     CacheKey = <<"bundler-msg-", (hb_util:human_id(ProcID))/binary, "-", (hb_util:bin(Slot))/binary>>,
     io:format("GET_MSG: Looking for key: ~p~n", [CacheKey]),
     case hb_cache:read(CacheKey, Opts) of
         {ok, BundlerMsgBinary} when is_binary(BundlerMsgBinary) -> 
             io:format("FOUND IN BUNDLER CACHE~n"),
-            % Deserialize the term
+						% Deserialize the term
             BundlerMsg = binary_to_term(BundlerMsgBinary),
             io:format("Msg: ~p~n", [BundlerMsg]),
             {ok, BundlerMsg};
@@ -23,147 +22,108 @@ get_message(Msg1, Msg2, Opts) ->
             dev_scheduler_cache:read(ProcID, Slot, Opts)
     end.
 
+request_cu(ProcID, Slot, Body, AOS2, Opts)->
+    hb_ao:resolve(
+      #{
+	<<"device">> => <<"relay@1.0">>,
+	<<"content-type">> => <<"application/json">>
+       },
+      AOS2#{
+	    <<"path">> => <<"call">>,
+	    <<"relay-method">> => <<"POST">>,
+	    <<"relay-body">> => Body,
+	    <<"relay-path">> =>
+		<< "/weavedb/", (hb_util:bin(Slot))/binary, "?process-id=", ProcID/binary >>,
+	    <<"content-type">> => <<"application/json">>
+	   },
+      Opts#{
+	    hashpath => ignore,
+	    cache_control => [<<"no-store">>, <<"no-cache">>]
+	   }
+     ).
+
+get_data(Res, Opts)->
+    ResultJSON = hb_ao:get(<<"body">>, Res, Opts),
+    Data = try dev_codec_json:from(ResultJSON) of
+	       Result -> dev_codec_json:from(ResultJSON),
+			 case Result of
+			     #{ <<"Output">> := #{ <<"data">> := D } } -> D;
+			     #{ <<"data">> := D } -> D;
+			     DirectData -> DirectData
+			 end
+	   catch
+	       _Err -> #{}
+	   end.
+
+get_result(Res, ID, Msg2, Opts) ->
+    Data = get_data(Res, Opts),
+    FromProcess = case hb_ao:get([<<"body">>, <<"from-process">>], Msg2, not_found, Opts) of
+		      not_found -> hb_ao:get(<<"from-process">>, Msg2, not_found, Opts);
+		      FoundFrom -> FoundFrom
+		  end,
+    Reference = case hb_ao:get([<<"body">>, <<"reference">>], Msg2, not_found, Opts) of
+		    not_found -> hb_ao:get(<<"reference">>, Msg2, not_found, Opts);
+		    FoundRef -> FoundRef
+		end,
+    case {FromProcess, Reference} of
+	{not_found, _} -> #{ <<"data">> => jsx:encode(Data) };
+	{_, not_found} ->  #{ <<"data">> => jsx:encode(Data) };
+	{FromProc, Ref} ->
+	    DataJSON = case Data of
+			   List when is_list(List) -> jsx:encode(Data);
+			   Map when is_map(Map) -> jsx:encode(Data);
+			   _ -> jsx:encode(Data)
+		       end,
+	    Outbox = #{
+		       <<"target">> => FromProc,
+		       <<"data">> => DataJSON,
+		       <<"x-reference">> => Ref,
+		       <<"type">> => <<"Message">>,
+		       <<"from-db">> => ID
+		      },
+
+	    #{ 
+	       <<"data">> => jsx:encode(Data),
+	       <<"outbox">> => #{ <<"1">> => Outbox }
+	     }
+    end.
+
 compute(Msg1, Msg2, Opts) ->
     ActionRaw = hb_ao:get([<<"body">>, <<"Action">>], Msg2, Opts),
     Action = case ActionRaw of
-        <<"\"Query\", \"Query\"">> -> <<"Query">>;
-        _ -> ActionRaw
-    end,
+		 <<"\"Query\", \"Query\"">> -> <<"Query">>;
+		 _ -> ActionRaw
+	     end,
+    Slot = hb_ao:get(<<"slot">>, Msg2, Opts),
+    ProcID = hb_ao:get(<<"process">>, Msg2, Opts),
+    ID = hb_ao:get(<<"db">>, Msg1, Opts),
     case Action of
         <<"Query">> ->
-            Slot = hb_ao:get(<<"slot">>, Msg2, Opts),
-            ProcID = hb_ao:get(<<"process">>, Msg2, Opts),
-
             {ok, AOS2 = #{ <<"body">> := Body }} =
                 dev_scheduler_formats:assignments_to_aos2(
-                    ProcID,
-                    #{
-                        Slot => Msg2
-                    },
-                    false,
-                    Opts
-                ),
-            {ok, Res} = 
-                hb_ao:resolve(
-                    #{
-                        <<"device">> => <<"relay@1.0">>,
-                        <<"content-type">> => <<"application/json">>
-                    },
-                    AOS2#{
-                        <<"path">> => <<"call">>,
-                        <<"relay-method">> => <<"POST">>,
-                        <<"relay-body">> => Body,
-                        <<"relay-path">> =>
-                            << "/weavedb/", (hb_util:bin(Slot))/binary, "?process-id=", ProcID/binary >>,
-                        <<"content-type">> => <<"application/json">>
-                    },
-                    Opts#{
-                        hashpath => ignore,
-                        cache_control => [<<"no-store">>, <<"no-cache">>]
-                    }
-                ),
-            ID = hb_ao:get(<<"db">>, Msg1, Opts),
-            ZKHash = hb_ao:get(<<"zkhash">>, Msg1, Opts),
-            
-            % Parse the JSON response to extract the data
-            ResultJSON = hb_ao:get(<<"body">>, Res, Opts),
-            Result = dev_codec_json:from(ResultJSON),
-            
-            % Extract just the data part
-            Data = case Result of
-                #{ <<"Output">> := #{ <<"data">> := D } } -> D;
-                #{ <<"data">> := D } -> D;
-                DirectData -> DirectData
-            end,
-            
-            % Check if we need to create an outbox response
-            FromProcess = case hb_ao:get([<<"body">>, <<"from-process">>], Msg2, not_found, Opts) of
-                not_found -> hb_ao:get(<<"from-process">>, Msg2, not_found, Opts);
-                FoundFrom -> FoundFrom
-            end,
-            Reference = case hb_ao:get([<<"body">>, <<"reference">>], Msg2, not_found, Opts) of
-                not_found -> hb_ao:get(<<"reference">>, Msg2, not_found, Opts);
-                FoundRef -> FoundRef
-            end,
-            Results = case {FromProcess, Reference} of
-                {not_found, _} ->
-                    % No from-process, just return data as before
-                    #{ <<"data">> => Data };
-                {_, not_found} ->
-                    % No reference, just return data as before
-                    #{ <<"data">> => Data };
-                {FromProc, Ref} ->
-                    % Both present, create outbox message as a reply
-                    % Encode the data based on its type
-                    DataJSON = case Data of
-                        List when is_list(List) -> 
-                            % For lists, encode directly
-                            jsx:encode(Data);
-                        Map when is_map(Map) ->
-                            % For maps, use dev_codec_json
-                            dev_codec_json:to(Data);
-                        _ ->
-                            % For other types, encode directly
-                            jsx:encode(Data)
-                    end,
-                    
-                    #{ 
-                        <<"data">> => Data,
-                        <<"outbox">> => #{
-                            <<"1">> => #{
-                                <<"target">> => FromProc,
-                                <<"data">> => DataJSON,
-                                <<"x-reference">> => Ref,
-                                <<"type">> => <<"Message">>,
-                                <<"from-db">> => ID
-                            }
-                        }
-                    }
-            end,
-            {ok, hb_ao:set(Msg1, #{ 
-                <<"db">> => ID, 
-                <<"zkhash">> => ZKHash, 
-                <<"results">> => Results 
-            }, Opts)};
+		  ProcID,
+		  #{
+		    Slot => Msg2
+		   },
+		  false,
+		  Opts
+		 ),
+            {ok, Res} = request_cu(ProcID, Slot, Body, AOS2, Opts),
+	    Results = get_result(Res, ID, Msg2, Opts),
+            {ok, hb_ao:set(Msg1, #{ <<"results">> => Results }, Opts)};
+	
         <<"Commit">> ->
-            ID = hb_ao:get(<<"db">>, Msg1, Opts),
+	    Body = hb_ao:get(<<"body">>, Msg2, Opts),
             ZKHash = hb_ao:get([<<"body">>,<<"zkhash">>], Msg2, Opts),
-            Slot = hb_ao:get(<<"slot">>, Msg2, Opts),
-            ProcID = hb_ao:get(<<"process">>, Msg2, Opts),
-            Body = hb_ao:get(<<"body">>, Msg2, Opts),
-	    {ok, Res} = 
-                hb_ao:resolve(
-                    #{
-                        <<"device">> => <<"relay@1.0">>,
-                        <<"content-type">> => <<"application/json">>
-                    },
-		    Msg2#{
-                        <<"path">> => <<"call">>,
-                        <<"relay-method">> => <<"POST">>,
-                        <<"relay-path">> =>
-                            << "/weavedb/", (hb_util:bin(Slot))/binary, "?process-id=", ProcID/binary >>,
-                        <<"content-type">> => <<"application/json">>
-                    },
-                    Opts#{
-                        hashpath => ignore,
-                        cache_control => [<<"no-store">>, <<"no-cache">>]
-                    }
-                ),
-            % Parse the JSON response to extract the data
-            ResultJSON = hb_ao:get(<<"body">>, Res, Opts),
-            Result = dev_codec_json:from(ResultJSON),
-            
-            % Extract just the data part
-            Data = case Result of
-                #{ <<"Output">> := #{ <<"data">> := D } } -> D;
-                #{ <<"data">> := D } -> D;
-                DirectData -> DirectData
-            end,
-            {ok, hb_ao:set(Msg1, #{ <<"db">> => ID, <<"zkhash">> => ZKHash, <<"results">> => #{ <<"data">> => dev_codec_json:to(Data) }}, Opts)};
+	    {ok, Res} = request_cu(ProcID, Slot, Body, Msg2, Opts),
+	    Results = get_result(Res, ID, Msg2, Opts),
+	    {ok, hb_ao:set(Msg1, #{ <<"zkhash">> => ZKHash, <<"results">> => Results}, Opts)};
+
         _Other ->
-            ID = hb_ao:get(<<"db">>, Msg1, Opts),
-            ZKHash = hb_ao:get([<<"body">>,<<"zkhash">>], Msg2, Opts),
-            {ok, hb_ao:set(Msg1, #{ <<"db">> => ID, <<"zkhash">> => ZKHash }, Opts)}
+	    Body = hb_ao:get(<<"body">>, Msg2, Opts),
+	    {ok, Res} = request_cu(ProcID, Slot, Body, Msg2, Opts),
+	    Results = get_result(Res, ID, Msg2, Opts),
+	    {ok, hb_ao:set(Msg1, #{ <<"results">> => Results}, Opts)}
     end.
 
 
@@ -185,112 +145,100 @@ start(_, _, Opts) ->
     JSON = dev_codec_json:to(#{ <<"status">> => ensure_started(Opts)}),
     {ok, #{ <<"content-type">> => <<"application/json">>, <<"body">> => JSON }}.
 
-%% @doc Check if the weavedb server is started without attempting to start it.
 is_started(Opts) ->
     Status = status(Opts),
     JSON = dev_codec_json:to(#{ <<"status">> => Status}),
     {ok, #{ <<"content-type">> => <<"application/json">>, <<"body">> => JSON }}.
 
-%% @doc Ensure the local `weavedb@1.0' is live. If it not, start it.
 ensure_started(Opts) ->
-    % Check if the `weavedb@1.0' device is already running. The presence
-    % of the registered name implies its availability.
     ?event({ensure_started, weavedb, self()}),
     IsRunning = is_weavedb_server_running(Opts),
     IsCompiled = hb_features:weavedb(),
     GenWDBProc = is_pid(hb_name:lookup(<<"weavedb@1.0">>)),
     case IsRunning orelse (IsCompiled andalso GenWDBProc) of
         true ->
-            % If it is, do nothing.
             true;
         false ->
-            % The device is not running, so we need to start it.
             PID =
                 spawn(
-                    fun() ->
-                        ?event({weavedb_booting, {pid, self()}}),
-                        % Create weavedb cache dir, if it does not exist.
-                        NodeURL =
-                            "http://localhost:" ++
-                            integer_to_list(hb_opts:get(port, no_port, Opts)),
-                        Port =
-                            open_port(
+		  fun() ->
+			  ?event({weavedb_booting, {pid, self()}}),
+			  NodeURL =
+			      "http://localhost:" ++
+			      integer_to_list(hb_opts:get(port, no_port, Opts)),
+			  Port =
+			      open_port(
                                 {spawn_executable,
-                                    "_build/weavedb-server/launch-monitored.sh"
+				 "_build/weavedb-server/launch-monitored.sh"
                                 },
                                 [
-                                    binary, use_stdio, stderr_to_stdout,
-                                    {args, [
-                                        "npm",
-                                        "--prefix",
-                                        "_build/weavedb-server",
-                                        "run",
-                                        "dev"
-                                    ]},
-                                    {env,
-                                        [
-                                        ]
-                                    }
+				 binary, use_stdio, stderr_to_stdout,
+				 {args, [
+					 "npm",
+					 "--prefix",
+					 "_build/weavedb-server",
+					 "run",
+					 "dev"
+					]},
+				 {env,
+				  [
+				  ]
+				 }
                                 ]
-                            ),
-                        ?event({weavedb_port_opened, {port, Port}}),
-                        collect_events(Port)
-                    end
-                ),
+			       ),
+			  ?event({weavedb_port_opened, {port, Port}}),
+			  collect_events(Port)
+		  end
+		 ),
             hb_name:register(<<"weavedb@1.0">>, PID),
             ?event({weavedb_starting, {pid, PID}}),
-            % Wait for the device to start.
             hb_util:until(
-                fun() ->
-                    receive after 2000 -> ok end,
-                    Status = is_weavedb_server_running(Opts),
-                    ?event({weavedb_boot_wait, {received_status, Status}}),
-                    Status
-                end
-            ),
+	      fun() ->
+		      receive after 2000 -> ok end,
+		      Status = is_weavedb_server_running(Opts),
+		      ?event({weavedb_boot_wait, {received_status, Status}}),
+		      Status
+	      end
+	     ),
             ?event({weavedb_started, {pid, PID}}),
             true
     end.
 
-%% @doc Check if the weavedb server is running, using the cached process ID
-%% if available.
 is_weavedb_server_running(Opts) ->
     case get(weavedb_pid) of
         undefined ->
             ?event(weavedb_pinging_server),
             Parent = self(),
             PID = spawn(
-                fun() ->
-                    ?event({weavedb_get_info_endpoint, {worker, self()}}),
-                    Parent ! {ok, self(), status(Opts)}
-                end
-            ),
+		    fun() ->
+			    ?event({weavedb_get_info_endpoint, {worker, self()}}),
+			    Parent ! {ok, self(), status(Opts)}
+		    end
+		   ),
             receive
                 {ok, PID, Status} ->
                     put(weavedb_pid, Status),
                     ?event({weavedb_received_status, Status}),
                     Status
             after ?STATUS_TIMEOUT ->
-                ?event({weavedb_status_check, timeout}),
-                erlang:exit(PID, kill),
-                false
+		    ?event({weavedb_status_check, timeout}),
+		    erlang:exit(PID, kill),
+		    false
             end;
         _ -> true
     end.
 
-%% @doc Check if the weavedb server is running by requesting its status
-%% endpoint.
 status(Opts) ->
     ServerPort = hb_opts:get(weavedb_port, 6364, Opts),
     Peer = <<"http://localhost:", (integer_to_binary(ServerPort))/binary>>,
     Path = <<"/status">>,
     Args = #{
-        peer => Peer,
-        path => Path,
-        method => <<"GET">>,
-        headers => #{},
-        body => <<>>
-    },
+	     peer => Peer,
+	     path => Path,
+	     method => <<"GET">>,
+	     headers => #{},
+	     body => <<>>
+	    },
     try hb_http_client:req(Args, Opts) of
         {ok, 200, _Headers, _Body} ->
             ?event({weavedb_status_check, {status, 200}}),
@@ -307,31 +255,24 @@ status(Opts) ->
             false
     end.
 
-%% @doc Collect events from the port and log them.
+
 collect_events(Port) ->
     collect_events(Port, <<>>).
 collect_events(Port, Acc) ->
     receive
         {Port, {data, Data}} ->
             collect_events(Port,
-                log_server_events(<<Acc/binary, Data/binary>>)
-            );
+			   log_server_events(<<Acc/binary, Data/binary>>)
+			  );
         stop ->
             port_close(Port),
             ?event(weavedb_stopped, {pid, self()}),
             ok
     end.
 
-%% @doc Log lines of output from the weavedb server.
 log_server_events(Bin) when is_binary(Bin) ->
     log_server_events(binary:split(Bin, <<"\n">>, [global]));
 log_server_events([Remaining]) -> Remaining;
 log_server_events([Line | Rest]) ->
     ?event(weavedb_server, {server_logged, Line}),
     log_server_events(Rest).
-
-%%% Tests
-resolve_add_test() ->
-    M1 = #{ <<"device">> => <<"weavedb@1.0">> },
-    M2 = #{ <<"path">> => <<"query">>, <<"a">> => 8, <<"b">> => 9 },
-    {ok, #{ <<"sum">> := 17 }} = hb_ao:resolve(M1, M2, #{}).
